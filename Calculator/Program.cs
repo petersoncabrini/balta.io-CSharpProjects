b020881519ff2b867f34c444b1c677ad72561ba6
Peterson <petersoncabrini@hotmail.com>
2022-11-16 08:34:24 -0300

    first commit / add .gitignore

---
+++ Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine(v1);
+    }
+}
+}
+
