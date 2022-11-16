e6587042bc04e01f4b6b764e827f870e045c113e
Peterson <petersoncabrini@hotmail.com>
2022-11-16 08:41:31 -0300

    adding sum operation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,11 +4,20 @@
 {
     private static void Main(string[] args)
     {
-        Console.Clear();
-        Console.WriteLine("Primeiro valor: ");
+        Console.WriteLine("Primeiro valor:");
         float v1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine());
 
-        Console.WriteLine(v1);
+        float resultado = v1 + v2;
+
+        Console.WriteLine("");
+
+        //Concatenação
+        Console.WriteLine("O resultado da soma é: " + resultado);
+
+        //Interpolação
+        Console.WriteLine($"O resultado da soma é {resultado}");
     }
 }
 }
