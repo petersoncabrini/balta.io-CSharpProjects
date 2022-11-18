c49ae3b779007c710228b8636e547cdcc0f1b8e0
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-18 08:04:24 -0300

    adding multiplication operation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,8 +4,8 @@
     {
         private static void Main(string[] args)
         {
-
         }
+
 
         static void soma()
         {
@@ -62,6 +62,23 @@
 
             Console.WriteLine($"O resultado da divisão é {resultado}");
 
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
             Console.ReadKey();
         }
     }
