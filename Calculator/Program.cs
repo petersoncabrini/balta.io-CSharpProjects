c909cd3f6286416b7423ab5e684e6cf3bc877cc5
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-19 20:08:45 -0300

    add menu functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -15,15 +15,23 @@
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisão");
             Console.WriteLine("4 - Multiplicação");
-            Console.WriteLine("5 - Sair");
             Console.WriteLine("-----------------");
             Console.WriteLine("Selecione uma opção: ");
 
             short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                default: Menu(); break;
+            }
         }
 
 
-        static void soma()
+        static void Soma()
         {
             Console.Clear();
 
@@ -43,6 +51,7 @@
             Console.WriteLine($"O resultado da soma é {resultado}");
 
             Console.ReadKey();
+            Menu();
         }
 
         static void Subtracao()
@@ -60,6 +69,7 @@
             Console.WriteLine($"Oresultado da subtracao é {resultado}");
 
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
@@ -79,6 +89,7 @@
             Console.WriteLine($"O resultado da divisão é {resultado}");
 
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
@@ -96,6 +107,7 @@
             float resultado = v1 * v2;
             Console.WriteLine($"O resultado da multiplicação é {resultado}");
             Console.ReadKey();
+            Menu();
         }
     }
 }
