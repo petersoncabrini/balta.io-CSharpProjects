b67bfee5d122ffaa42d8c17fe5ac3e2d64c4f01b
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-21 08:17:27 -0300

    add start function to stopwatch

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Start(6);
+    }
+
+    static void Start(int time)
+    {
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado.");
+        Thread.Sleep(2500);
+    }
+}
