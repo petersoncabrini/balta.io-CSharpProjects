f93ded2954916a6fe70dfd34d1b4d5d04fe2c22e
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-21 08:35:08 -0300

    add app menu

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -5,6 +5,21 @@
         Start(6);
     }
 
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundos - Ex: 10s (10 segundos)");
+        Console.WriteLine("M = Minutos - Ex: 1m (1 minuto)");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+
+        Console.ReadLine();
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring((data.Length - 1), 1));
+        int time = int.Parse(data.Substring(0, (data.Length - 1)));
+    }
+
     static void Start(int time)
     {
         int currentTime = 0;
