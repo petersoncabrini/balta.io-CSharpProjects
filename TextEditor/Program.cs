c6dde20ac93cd8b888dd11546cd408c3b1db1d70
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-26 19:01:14 -0300

    add new project

---
+++ TextEditor/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que voce deseja fazer?");
+        Console.WriteLine("1- Abrir arquivo");
+        Console.WriteLine("2- Criar novo arquivo");
+        Console.WriteLine("0- Sair");
+        short option = short.Parse(Console.Readline());
+    }
+}
