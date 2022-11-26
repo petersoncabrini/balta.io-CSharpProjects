a841dfbf58575817208a3813539243888a2051f4
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-26 19:14:53 -0300

    add menu options and exit function

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-
+        Menu();
     }
 
     static void Menu()
@@ -12,6 +12,18 @@
         Console.WriteLine("1- Abrir arquivo");
         Console.WriteLine("2- Criar novo arquivo");
         Console.WriteLine("0- Sair");
-        short option = short.Parse(Console.Readline());
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0: System.Environment.Exit(0); break;
+            case 1: Abrir(); break;
+            case 2: Criar(); break;
+            default: Menu(); break;
+        }
     }
+
+    static void Abrir() { }
+
+    static void Criar() { }
 }
